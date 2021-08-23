3d9b2dc02134220134ed68485d4d203a1630b6fd
hAarniskagekko <haarniskagekko@gmail.com>
2021-08-23 19:45:25 +0300

    Add login

--- cc_mooc_proj1/cc_mooc_proj1/urls.py
+++ cc_mooc_proj1/cc_mooc_proj1/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/', LoginView.as_view(template_name='cc_mooc_proj1/login.html')),
     path('', views.index, name='index'),
     path('tweet_passwd/', views.tweet_passwd, name='tweet_passwd'),
     path('post_tweet/', views.post_tweet, name='post_tweet'),
--- cc_mooc_proj1/cc_mooc_proj1/views.py
+++ cc_mooc_proj1/cc_mooc_proj1/views.py
@@ -1,8 +1,10 @@
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 from .forms import TweetPwForm
 from .models import Tweet
 
+@login_required(login_url='login/')
 def index(request):
 	return render(request, 'cc_mooc_proj1/index.html')
 
@@ -14,7 +16,7 @@ def tweet_passwd(request):
 def post_tweet(request):
 	form = TweetPwForm(request.POST)
 	form.is_valid()
-	
+
 	tweet = Tweet(content=form.cleaned_data['tweet_content'])
 	tweet.save()
 
